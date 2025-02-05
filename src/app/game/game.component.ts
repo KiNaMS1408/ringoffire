9497586ce8a01cc02c58f8f758e551d27beb9421
Franz Neußer <140851101+KiNaMS1408@users.noreply.github.com>
2025-02-05 17:38:32 +0100

    Cards and players ground struktur implement

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/models/game';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
+  picCardAnimation = false;
+  currentCard: string = '';
+  game!: Game;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.newGame();
+
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+  }
+
+  takeCard() {
+    if (!this.picCardAnimation) {
+      this.currentCard = this.game.stack.pop() || '';
+      this.picCardAnimation = true;
+      console.log('Game is ' + this.game.playedCards);
+      
+      setTimeout(() => {
+        this.game.playedCards.push(this.currentCard);
+        this.picCardAnimation = false;
+      }, 1000);
+    }
+  }
+
 
 }
