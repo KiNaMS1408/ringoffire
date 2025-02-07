8eb43fa5c882f53e87ecb84fec57ffb7fa641ad6
Franz Neußer <140851101+KiNaMS1408@users.noreply.github.com>
2025-02-07 17:32:46 +0100

    First playable version
    Reade to play at one screen

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Game } from 'src/models/game';
+import { MatDialog, MAT_DIALOG_DATA, MatDialogRef, MatDialogModule } from '@angular/material/dialog';
+import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
 
 @Component({
   selector: 'app-game',
@@ -7,34 +9,52 @@ import { Game } from 'src/models/game';
   styleUrls: ['./game.component.scss']
 })
 export class GameComponent implements OnInit {
-  picCardAnimation = false;
+  pickCardAnimation = false;
   currentCard: string = '';
   game!: Game;
+  gameOver = false;
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.newGame();
-
   }
 
   newGame() {
     this.game = new Game();
-    console.log(this.game);
   }
 
   takeCard() {
-    if (!this.picCardAnimation) {
+    if (this.game.stack.length == 0) {
+      this.gameOver = true;
+    } else 
+    if (!this.pickCardAnimation) {
       this.currentCard = this.game.stack.pop() || '';
-      this.picCardAnimation = true;
-      console.log('Game is ' + this.game.playedCards);
+      this.pickCardAnimation = true;
+      console.log('Curret Game Card is 34 ', this.currentCard);
       
+      this.game.currentPlayer++;
+      this.game.currentPlayer = this.game.currentPlayer % this.game.players.length;
+
       setTimeout(() => {
         this.game.playedCards.push(this.currentCard);
-        this.picCardAnimation = false;
+  
+        this.pickCardAnimation = false;
       }, 1000);
     }
   }
+  
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddPlayerComponent);
 
+    dialogRef.afterClosed().subscribe((name:string) => {
+      if (name && name.length > 0) {
+      this.game.players.push(name)
+      console.log('Game is 50 ', name  );
+      }
+    });
+  }
 
 }
+
+
